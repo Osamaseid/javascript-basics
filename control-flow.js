5ce5db172982607bde6b9f24c7155a0a158050cb
osama seid <osaseid1@gmail.com>
2024-02-26 12:05:02 +0300

    About control-flow

---
+++ control-flow.js
@@ -0,0 +1,22 @@
+// Example of if-else statement
+let hour = 12;
+if (hour < 12) {
+  console.log("Good morning!");
+} else if (hour < 18) {
+  console.log("Good afternoon!");
+} else {
+  console.log("Good evening!");
+}
+
+// Example of for loop
+
+for (let i = 0; i < 5; i++) {
+  console.log(i);
+}
+
+// Example of function declaration
+
+function greet(name) {
+  console.log("Hello, " + name + "!");
+}
+greet("osama");git
