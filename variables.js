5007dcdf05279b2a70bf0c627399750ab64e6b2d
osama seid <osaseid1@gmail.com>
2024-02-26 11:45:27 +0300

    git

---
+++ variables.js
@@ -0,0 +1,11 @@
+// Example of declaring variables
+var name = "mohammed";
+let age = 21;
+const height = 1.8;
+
+// Example of different data types
+let message = "Hello, world!";
+let count = 10;
+let isTrue = true;
+let fruits = ["apple", "banana", "orange"];
+let person = { name: "mohammed", age: 21 };
