a849ee199196df460d2df7bb033c61e25af53b05
osama seid <osaseid1@gmail.com>
2024-02-27 17:42:55 +0300

    Control-flow in javascript

---
+++ Control-flow.js
@@ -0,0 +1,28 @@
+// Control flow example using switch
+let day = "Wednesday"; // Represents the day of the week
+
+switch (day) {
+  case "Monday":
+    console.log("It's Monday");
+    break;
+  case "Tuesday":
+    console.log("It's Tuesday");
+    break;
+  case "Wednesday":
+    console.log("It's Wednesday");
+    break;
+  case "Thursday":
+    console.log("It's Thursday");
+    break;
+  case "Friday":
+    console.log("It's Friday");
+    break;
+  case "Saturday":
+    console.log("It's Saturday");
+    break;
+  case "Sunday":
+    console.log("It's Sunday");
+    break;
+  default:
+    console.log("Invalid day");
+}
