f3ec6c372cb90ff8df5b0df83a616c6d11484d35
osama seid <osaseid1@gmail.com>
2024-02-27 17:40:14 +0300

    something wrong

--- array.js
+++ array.js
@@ -1,25 +1,13 @@
 // Example of manipulating arrays
 let fruits = ["apple", "banana", "orange"];
-fruits.push("grape");
+fruits.push("habhab");
 fruits.pop();
 fruits.shift();
-fruits.unshift("pear");
+fruits.unshift("temer");
 fruits.splice(1, 1);
 
 // Example of array iteration
+
 fruits.forEach(function(fruit) {
   console.log(fruit);
 });
-
-let numbers = [1, 2, 3, 4, 5];
-let doubledNumbers = numbers.map(function(number) {
-  return number * 2;
-});
-
-let filteredNumbers = numbers.filter(function(number) {
-  return number > 2;
-});
-
-let sum = numbers.reduce(function(total, number) {
-  return total + number;
-}, 0);
---
+++ function.js
@@ -0,0 +1,9 @@
+
+// example about function
+function calculator(num1, num2){
+    let add;
+    add = num1 + num2;
+    return add;
+}
+var adder = calclatotr();
+console.log(adder);
